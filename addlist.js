13b06d26172a3c50d7a1d59094605335398bfca3
EwweKovacevic <70662286+EwweKovacevic@users.noreply.github.com>
2022-08-30 10:44:52 +0200

    Update addlist.js

--- addlist.js
+++ addlist.js
@@ -1,12 +1,12 @@
 const li1 = document.getElementById('li1');
-const inputID = document.getElementById('inputID');
+const inputID = document.getElementById('inputID').value;
 
 // ✅ Toggle button text on click
 li1.addEventListener('click', function handleClick() {
   const initialText = '';
 
   if (li1.textContent.toLowerCase().includes(initialText.toLowerCase())) {
-    li1.textContent = 'inputID.textContent';
+    li1.textContent = 'inputID';
   } else {
     li1.textContent = initialText;
   }
